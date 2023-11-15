66e17ac8d9d608b8129bee466b824bd44a0c6a75
svist <svistak.91@bk.ru>
2023-11-15 17:07:00 +0500

    version 1.1.1

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-
-from flask import Flask, render_template, request, flash,redirect,url_for
-from flask_login import LoginManager, UserMixin, login_required,login_user
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -29,36 +28,52 @@ class Profiles(db.Model):
     city = db.Column(db.String(20), nullable=False)
     Username = db.Column(db.String(20), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    description=db.Column(db.Text(), nullable=True)
+    age=db.Column(db.String(10),nullable=True)
 
     def __repr__(self):
         return '<Profiles %r>' % self.id
 
-
-# @login_manager.user_loader
-# def load_user(user_id):
-#     print('load_user')
-#     return UserLogIn().fromDB(user_id, db)
 
 @login_manager.user_loader
 def load_user(user_id):
     return db.session.query(User).get(user_id)
 
 
-# @app.route("/")
-# @app.route("/index")
-# def index():
-#     return render_template("index.html")
+@app.route("/index")
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+#@app.route('/submit_profile',methods=["GET", "POST"])
+#def submit_profile():
 
 
 @app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         user = User.query.filter_by(
             email=request.form.get("email")).first()
-        if check_password_hash(user.Password, request.form.get("password")):
-            login_user(user)
-            return redirect(url_for("personal_account"))
+        if user:
+            # profile = Profiles.query.where(user.id == Profiles.user_id).first()
+            if check_password_hash(user.Password, request.form.get("password")):
+                login_user(user)
+                # if profile.description == None:
+                #     return redirect(url_for("submit_profile"))
+                return redirect(url_for("personal_account"))
+            else:
+                flash('Неверный адрес электронной почты/пароль', category='error')
+        else:
+            flash('Неверный адрес электронной почты/пароль', category='error')
     return render_template("index.html")
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 @app.route("/personal_account")
@@ -75,7 +90,7 @@ def signup():
                 email=request.form.get("email")).first()
             if chek_user:
                 flash("Ошибка регистрации", category='error')
-                flash('Пароли должны совпадать', category='error')
+                flash('Пользователь с таким email уже зарегестрирован', category='error')
                 return render_template("signup.html")
             username = request.form['username']
             # шифрование паролей чтоб не сглазили
